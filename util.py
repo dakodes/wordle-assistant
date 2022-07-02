688a33a2940f6310fc6e2020dcee10ad90b3bbb2
David Akodes <dakodes@users.noreply.github.com>
2022-07-02 16:13:49 -0400

    Fix the determination of the Javascript script URL for the current web version of Wordle

--- util.py
+++ util.py
@@ -6,21 +6,20 @@ import requests
 
 
 def download_answers() -> List[str]:
-    wordle_base_url = "https://www.nytimes.com/games/wordle"
-
-    # Download the index page and find the Javascript script name
-    wordle_index_url = f"{wordle_base_url}/index.html"
+    # Download the index page and find the Javascript script url
+    wordle_index_url = "https://www.nytimes.com/games/wordle/index.html"
     wordle_index_download_response = requests.get(wordle_index_url)
     if wordle_index_download_response.status_code != requests.codes["ok"]:
         raise RuntimeError("Failed to download the Wordle index page")
-    javascript_script_name_re_results = re.findall(r'<script src="(main.[a-z\d]+.js)"></script>',
+    # src="https://www.nytimes.com/games-assets/v2/wordle.6b88b1c8c6541b07820ab0896f6bc19be0ae34b2.js"
+    javascript_script_url_re_results = re.findall(r'https:\/\/www\.nytimes\.com\/games-assets\/v2\/wordle\.[a-f0-9]+\.js',
                                                    wordle_index_download_response.text)
-    if not javascript_script_name_re_results:
-        raise RuntimeError("Failed to parse the Javascript script name from the downloaded Wordle index page")
-    if len(javascript_script_name_re_results) != 1:
-        raise RuntimeError(f"Expected to find exactly 1 Javascript script name in the downloaded Wordle index page"
-                           f"but found {len(javascript_script_name_re_results)}")
-    wordle_javascript_url = f"{wordle_base_url}/{javascript_script_name_re_results[0]}"
+    if not javascript_script_url_re_results:
+        raise RuntimeError("Failed to parse the Javascript script url from the downloaded Wordle index page")
+    if len(javascript_script_url_re_results) != 1:
+        raise RuntimeError(f"Expected to find exactly 1 Javascript script url in the downloaded Wordle index page"
+                           f"but found {len(javascript_script_url_re_results)}")
+    wordle_javascript_url = javascript_script_url_re_results[0]
 
     # Download the Javascript script and find the answers list
     wordle_javascript_download_response = requests.get(wordle_javascript_url)
